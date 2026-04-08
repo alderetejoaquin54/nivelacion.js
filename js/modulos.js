4a6f3339c9ed637fa5191671e81ff886c118e00d
Joaquin <alderetejoaquin54@gmail.com>
2026-04-08 20:15:26 -0300

    modulos

---
+++ js/modulos.js
@@ -0,0 +1,6 @@
+const header = '<strong><i> Mi primer pagina con modulos </i></strong>';
+const nav = '<a>Index</a><a>Productos</a><a>Servicios</a>';
+const footer = 'Joaquin Alderete, Nicolas Corbalan';
+document.querySelector('header').innerHTML= header;
+document.querySelector('#menu').innerHTML= nav;
+document.querySelector('.Pie').innerHTML= footer;
