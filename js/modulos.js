61be7cc3129da9ffdb59e45326cbb30fdbddbb6c
CalazaSantiago <calaza.santiago@tecnica7.edu.ar>
2026-04-13 20:25:47 -0300

    arreglo

--- js/modulos.js
+++ js/modulos.js
@@ -1,5 +1,5 @@
 const header = '<strong><i> Mi primer pagina con modulos </i></strong>';
-const nav = '<a>Index</a><a>Productos</a><a>Servicios</a>';
+const nav = '<ul><li><a href="index.html">Index</a></li><li><a href="productos.html">Productos</a></li><li><a href="servicios.html">Servicios</a></li></ul>';
 const footer = 'Joaquin Alderete, Nicolas Corbalan';
 document.querySelector('header').innerHTML= header;
 document.querySelector('#menu').innerHTML= nav;
